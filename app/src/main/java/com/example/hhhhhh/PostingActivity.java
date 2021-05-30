8bac84c6a7a0f21bdfaa765082d03e74ab771aaa
leedh0209 <ldh02091877@gmail.com>
2021-05-30 22:42:16 +0900

    거래탭 구현

--- app/src/main/java/com/example/hhhhhh/PostingActivity.java
+++ app/src/main/java/com/example/hhhhhh/PostingActivity.java
@@ -1,30 +1,233 @@
 package com.example.hhhhhh;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Fragment;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Date;
 
 public class PostingActivity extends AppCompatActivity {
-    Button posting_btn;
-    Fragment3 fragment3;
+    private Button posting_btn;
+    private Button posting_image;
+    private FirebaseUser user;
+    private FirebaseFirestore db;
+    private EditText titleEditText;
+    private Spinner isSellingSpinner;
+    private Spinner sellCategorySpinner;
+    private EditText priceEditText;
+    private EditText phoneEditText;
+    private EditText postContentEditText;
+    private DatabaseReference mDatabase;
+    private String school = "gachon";
+    private String sellerName = "tester";
+    private String imagePath;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sellposting);
 
+        final String[] isSell = {"판매", "교환"};
+        final String[] categ = {"식품", "잡동사니"};
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        titleEditText = findViewById(R.id.posting_title);
+        isSellingSpinner = findViewById(R.id.category);
+        sellCategorySpinner = findViewById(R.id.sell_category);
+        priceEditText = findViewById(R.id.posting_price);
+        phoneEditText = findViewById(R.id.posting_phone);
+        postContentEditText = findViewById(R.id.posting_content);
+        imageView = findViewById(R.id.posting_img);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, isSell);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        isSellingSpinner.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, categ);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sellCategorySpinner.setAdapter(adapter2);
+
+
         posting_btn = findViewById(R.id.posting_btn);
         posting_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fragment3 = new Fragment3();
+                upLoad();
+            }
+        });
 
-                getSupportFragmentManager().beginTransaction().add(R.id.lv4, fragment3).commit();
+        posting_image = findViewById(R.id.posting_image_btn);
+        posting_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GalleryActivity.class);
+                startActivityForResult(intent, 0);
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 0:
+                if (resultCode == Activity.RESULT_OK) {
+                    imagePath = data.getStringExtra("path");
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    imageView.setAdjustViewBounds(true);
+
+                    Glide.with(this).load(imagePath).override(1000).into(imageView);
+                }
+                break;
+        }
+    }
+
+    private void upLoad() {
+        if(imagePath != null) {
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            String[] patharray = imagePath.split("\\.");
+            final StorageReference mountainImagesRef = storageRef.child("users/" + user.getUid() + "/" + 0 + "." + patharray[patharray.length - 1]);
+            try {
+                InputStream stream = new FileInputStream(new File(imagePath));
+                StorageMetadata metadata = new StorageMetadata.Builder()
+                        .setCustomMetadata("index", "" + (0)).build();
+
+                UploadTask uploadTask = mountainImagesRef.putStream(stream, metadata);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        final int index = Integer.parseInt(taskSnapshot.getMetadata().getCustomMetadata("index"));
+
+                        mountainImagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                final String title = titleEditText.getText().toString();
+                                CollectionReference collection = db.collection("User");
+                                collection.document(user.getUid()).get()
+                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if(task.isSuccessful()){
+                                                    DocumentSnapshot document = task.getResult();
+                                                    school = document.getData().get("school").toString();
+                                                    sellerName = document.getData().get("name").toString();
+                                                }
+                                                if (title.length() > 0) {
+                                                    String price = priceEditText.getText().toString();
+                                                    String sellerUid = user.getUid();
+                                                    boolean isSelling = (isSellingSpinner.getSelectedItem().toString().equals("판매"));
+                                                    String category = sellCategorySpinner.getSelectedItem().toString();
+                                                    String phone = phoneEditText.getText().toString();
+                                                    String content = postContentEditText.getText().toString();
+
+                                                    SellItem trade = new SellItem(title, school, price, sellerUid, sellerName, isSelling, category, phone, content, new Date(), uri.toString());
+
+                                                    storeUpload(trade);
+                                                }
+                                            }
+                                        });
+                            }
+                        });
+                    }
+                });
+            } catch (FileNotFoundException e) {
+                Log.e("로그", "에러: " + e.toString());
+            }
+        }else{
+            final String title = titleEditText.getText().toString();
+            CollectionReference collection = db.collection("User");
+            collection.document(user.getUid()).get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()){
+                                DocumentSnapshot document = task.getResult();
+                                school = document.getData().get("school").toString();
+                                sellerName = document.getData().get("name").toString();
+                            }
+                            if (title.length() > 0) {
+                                String price = priceEditText.getText().toString();
+                                String sellerUid = user.getUid();
+                                boolean isSelling = (isSellingSpinner.getSelectedItem().toString().equals("판매"));
+                                String category = sellCategorySpinner.getSelectedItem().toString();
+                                String phone = phoneEditText.getText().toString();
+                                String content = postContentEditText.getText().toString();
+
+                                SellItem trade = new SellItem(title, school, price, sellerUid, sellerName, isSelling, category, phone, content, new Date(), "");
+
+                                storeUpload(trade);
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void storeUpload (SellItem trade){
+        db.collection("Trade")
+                .add(trade)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "거래 생성 성공", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "거래 생성 실패", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/example/hhhhhh/SellItem.java
+++ app/src/main/java/com/example/hhhhhh/SellItem.java
@@ -3,49 +3,103 @@ package com.example.hhhhhh;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 
-public class SellItem {
-   int image;
-  String price;
+import java.io.Serializable;
+import java.util.Date;
+
+public class SellItem implements Serializable {
+    String price;
     String title;
     String school;
+    String sellerUid;
+    String sellerName;
+    boolean isSelling;
+    String category;
+    String phone;
+    String content;
+    Date createdAt;
+    String id;
+    String img;
 
 
-    public SellItem(int image, String title, String school, String price) {
-        this.image = image;
+    public SellItem(String title, String school, String price, String sellerUid, String sellerName, boolean isSelling, String category, String phone, String content, Date createdAt, String img) {
         this.title = title;
         this.school = school;
         this.price = price;
+        this.sellerUid = sellerUid;
+        this.sellerName = sellerName;
+        this.isSelling = isSelling;
+        this.category = category;
+        this.phone = phone;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.img = img;
     }
 
-    public int getImage() {
-        return image;
+    public SellItem(String title, String school, String price, String sellerUid, String sellerName, boolean isSelling, String category, String phone, String content, Date createdAt, String img, String id) {
+        this.title = title;
+        this.school = school;
+        this.price = price;
+        this.sellerUid = sellerUid;
+        this.sellerName = sellerName;
+        this.isSelling = isSelling;
+        this.category = category;
+        this.phone = phone;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.img = img;
+        this.id = id;
     }
 
-    public String getPrice() {
-        return price;
-    }
 
+    public String getPrice() { return price; }
     public String getSchool() {
         return school;
     }
-
     public String getTitle() {
         return title;
     }
-
-    public void setImage(int image) {
-        this.image = image;
+    public String getSellerUid() {
+        return sellerUid;
     }
+    public String getSellerName() {
+        return sellerName;
+    }
+    public boolean getIsSelling() { return isSelling; }
+    public String getCategory() {
+        return category;
+    }
+    public String getPhone(){return phone;}
+    public String getContent(){return content;}
+    public Date getDate(){return createdAt;}
+    public String getId() { return id; }
+    public String getImg(){return img;}
 
     public void setPrice(String price) {
         this.price = price;
     }
-
     public void setSchool(String school) {
         this.school = school;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
+    public void setSellerUid(String sellerUid) {
+        this.sellerUid = sellerUid;
+    }
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+    public void setIsSelling(boolean isSelling) { this.isSelling = isSelling; }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setphone(String phone) { this.phone = phone; }
+    public void setcontent(String content) {
+        this.content = content;
+    }
+    public void setDate(Date createdAt){this.createdAt = createdAt;}
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setImg(String img){this.img = img;}
 }
