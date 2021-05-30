8a5c2665d3546d68d421dbf599e21a6296084d7e
watervin <84309114+watervin@users.noreply.github.com>
2021-05-30 23:35:39 +0900

    김수빈수정

--- app/src/main/java/com/example/hhhhhh/reputa.java
+++ app/src/main/java/com/example/hhhhhh/reputa.java
@@ -1,31 +1,103 @@
 package com.example.hhhhhh;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class reputa extends AppCompatActivity {
+    private static final String TAG = "HomeFragment";
+    private FirebaseFirestore mFirebaseFirestore;
+    private InfoAdapter InfoAdapter;
+    private RecyclerView recyclerView;
+    FirebaseFirestore db;
 
+    public reputa() {
+        // Required empty public constructor
+    }
 
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reputation);
+    }
+
+
 /*
-        Button btn_return = findViewById(R.id.btn_return);
-        btn_return.setOnClickListener(new View.OnClickListener() {
+
+    void getData() {
+
+        mFirebaseFirestore.collection("Events").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "이전으로", Toast.LENGTH_LONG).show();
-                finish();
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<String> list = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        list.add(document.getId());
+                    }
+                    Log.d(TAG, list.toString());
+                    updateData((ArrayList) list); // *** new ***
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
             }
         });
 
- */
     }
+
+    void updateData(ArrayList list) {
+
+        // Get a new write batch
+
+        WriteBatch batch = db.batch();
+
+        // Iterate through the list
+        for (int k = 0; k < list.size(); k++) {
+
+            // Update each list item
+            DocumentReference ref = db.collection("Events").document(list.get(k));
+            batch.update(ref, "field_you_want_to_update", "new_value");
+
+        }
+
+        // Commit the batch
+        batch.commit().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                // Yay its all done in one go!
+            }
+        });
+
+    }
+
+
+
+
+
+*/
+
 }
--- app/src/main/java/com/example/hhhhhh/setting.java
+++ app/src/main/java/com/example/hhhhhh/setting.java
@@ -10,13 +10,15 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class setting extends AppCompatActivity {
-
+    private FirebaseAuth mAuth = null;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set);
-
+        mAuth = FirebaseAuth.getInstance();
 /*
         Button btn_return = findViewById(R.id.btn_return);
         btn_return.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +39,32 @@ public class setting extends AppCompatActivity {
                 builder.setMessage("로그아웃 하시겠습니까?");
                 builder.setPositiveButton("로그아웃", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent i = new Intent(setting.this/*현재 액티비티 위치*/ , LoginActivity.class/*이동 액티비티 위치*/);
+                        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(i);
+                    }
+                })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                            }
+                        })
+                        .show();
+
+            }
+        });
+        Button btn_Withdrawal = findViewById(R.id.btn_Withdrawal);
+        btn_Withdrawal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(setting.this);
+                builder.setTitle("탈퇴하기");
+                builder.setMessage("탈퇴 하시겠습니까?");
+                builder.setPositiveButton("탈퇴하기", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                        mAuth.getCurrentUser().delete();
                         Intent i = new Intent(setting.this/*현재 액티비티 위치*/ , LoginActivity.class/*이동 액티비티 위치*/);
                         i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
                         startActivity(i);
